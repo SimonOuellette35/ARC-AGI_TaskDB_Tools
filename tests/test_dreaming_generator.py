27d562ade22ec83043e3c2580bf8f7cbd600c29b
SimonOuellette35 <chiefscientist@outlook.com>
2026-01-20 14:27:18 -0500

    added unit test

--- tests/test_dreaming_generator.py
+++ tests/test_dreaming_generator.py
@@ -1,5 +1,6 @@
-from dreaming.dreaming_data_generator import DreamingDataGenerator
+from dreaming.dreaming_data_generator import DreamingDataGenerator, block_of_text_to_program_lines
 from dreaming.grid_example_generator import generate_grid_examples
+from AmotizedDSL.prog_utils import ProgUtils
 
 
 def test_compare_two_tasks():
@@ -92,106 +93,105 @@ def test_compare_two_tasks():
     # Example #2: two distinct tasks
 
     task1 = {
-    "program": "[\n  add(N+0.x, 1),\n  set_pixels(N+0, N+1, N+0.y, N+0.c),\n  del(N+1),\n  del(N+0),\n  set_pixels(N+0, 0, N+0.y, 0),\n  del(N+0),\n  crop(N+0, 0, 0, N+0.max_x, N+0.height),\n  del(N+0),\n  equal(N+0.c, param1),\n  equal(N+0.c, param2),\n  switch(N+1, N+2, param2, param1, N+0.c),\n  del(N+1),\n  del(N+1),\n  set_pixels(N+0, N+0.x, N+0.y, N+1),\n  del(N+0),\n  del(N+0)\n]",
-    "instructions": [
-      [0, 24, 1, 61, 52, 2, 5, 3],
-      [0, 38, 1, 61, 2, 62, 2, 61, 53, 2, 61, 54, 3],
-      [0, 51, 1, 62, 3],
-      [0, 51, 1, 61, 3],
-      [0, 38, 1, 61, 2, 4, 2, 61, 53, 2, 4, 3],
-      [0, 51, 1, 61, 3],
-      [0, 36, 1, 61, 2, 4, 2, 4, 2, 61, 55, 2, 61, 58, 3],
-      [0, 51, 1, 61, 3],
-      [0, 18, 1, 61, 54, 2, "param1", 3],
-      [0, 18, 1, 61, 54, 2, "param2", 3],
-      null,
-      [0, 51, 1, 62, 3],
-      [0, 51, 1, 62, 3],
-      [0, 38, 1, 61, 2, 61, 52, 2, 61, 53, 2, 62, 3],
-      [0, 51, 1, 61, 3],
-      [0, 51, 1, 61, 3]
-    ],
-    "comments": [
-      "x + 1",
-      "Grid",
-      "",
-      "",
-      "Grid",
-      "",
-      "Grid",
-      "",
-      "for each pixel, whether it has color 0",
-      "for each pixel, whether it has color 1",
-      "0 pixels set to 1, 1 pixels set to 0",
-      "",
-      "",
-      "Grid",
-      "",
-      ""
-    ],
-    "curriculum_level": 1,
-    "name": "Shift Right + SwapColor",
-    "source": "Manual",
-    "parameters": [
-      "existing_color",
-      "existing_color"
-    ],
-    "grid_categories": [
-      "basic"
-    ],
-    "min_grid_dim": 3,
-    "max_grid_dim": 30,
-    "validated": True
-  }
+        "program": "[\n  add(N+0.x, 1),\n  set_pixels(N+0, N+1, N+0.y, N+0.c),\n  del(N+1),\n  del(N+0),\n  set_pixels(N+0, 0, N+0.y, 0),\n  del(N+0),\n  crop(N+0, 0, 0, N+0.max_x, N+0.height),\n  del(N+0),\n  equal(N+0.c, param1),\n  equal(N+0.c, param2),\n  switch(N+1, N+2, param2, param1, N+0.c),\n  del(N+1),\n  del(N+1),\n  set_pixels(N+0, N+0.x, N+0.y, N+1),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 24, 1, 61, 52, 2, 5, 3],
+        [0, 38, 1, 61, 2, 62, 2, 61, 53, 2, 61, 54, 3],
+        [0, 51, 1, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 38, 1, 61, 2, 4, 2, 61, 53, 2, 4, 3],
+        [0, 51, 1, 61, 3],
+        [0, 36, 1, 61, 2, 4, 2, 4, 2, 61, 55, 2, 61, 58, 3],
+        [0, 51, 1, 61, 3],
+        [0, 18, 1, 61, 54, 2, "param1", 3],
+        [0, 18, 1, 61, 54, 2, "param2", 3],
+        [0, 21, 1, 62, 2, 63, 2, "param2", 2, "param1", 2, 61, 54, 3],
+        [0, 51, 1, 62, 3],
+        [0, 51, 1, 62, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 61, 53, 2, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "x + 1",
+        "Grid",
+        "",
+        "",
+        "Grid",
+        "",
+        "Grid",
+        "",
+        "for each pixel, whether it has color 0",
+        "for each pixel, whether it has color 1",
+        "0 pixels set to 1, 1 pixels set to 0",
+        "",
+        "",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 1,
+        "name": "Shift Right + SwapColor",
+        "source": "Manual",
+        "parameters": [
+        "existing_color",
+        "existing_color"
+        ],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
 
-  task2 = {
-    "program": "[\n  sub(N+0.y, 1),\n  set_pixels(N+0, N+0.x, N+1, N+0.c),\n  del(N+1),\n  del(N+0),\n  set_pixels(N+0, N+0.x, N+0.max_y, 0),\n  del(N+0),\n  crop(N+0, 0, 1, N+0.width, N+0.height),\n  del(N+0),\n  equal(N+0.c, param1),\n  switch(N+1, param1, param2),\n  del(N+1),\n  set_pixels(N+0, N+0.x, N+0.y, N+1),\n  del(N+0),\n  del(N+0)\n]",
-    "instructions": [
-      [0, 25, 1, 61, 53, 2, 5, 3],
-      [0, 38, 1, 61, 2, 61, 52, 2, 62, 2, 61, 54, 3],
-      [0, 51, 1, 62, 3],
-      [0, 51, 1, 61, 3],
-      [0, 38, 1, 61, 2, 61, 52, 2, 61, 56, 2, 4, 3],
-      [0, 51, 1, 61, 3],
-      [0, 36, 1, 61, 2, 4, 2, 5, 2, 61, 57, 2, 61, 58, 3],
-      [0, 51, 1, 61, 3],
-      [0, 18, 1, 61, 54, 2, "param1", 3],
-      [0, 21, 1, 62, 2, "param1", 2, "param2", 3],
-      [0, 51, 1, 62, 3],
-      [0, 38, 1, 61, 2, 61, 52, 2, 61, 53, 2, 62, 3],
-      [0, 51, 1, 61, 3],
-      [0, 51, 1, 61, 3]
-    ],
-    "comments": [
-      "y - 1",
-      "Grid",
-      "",
-      "",
-      "Grid",
-      "",
-      "Grid",
-      "",
-      "for each pixel, whether it has color 0",
-      "non-0 color pixels set to 1",
-      "Grid",
-      "",
-      ""
-    ],
-    "curriculum_level": 1,
-    "name": "Shift Up + OtherColors",
-    "source": "Manual",
-    "parameters": [
-      "existing_color",
-      "color"
-    ],
-    "grid_categories": [
-      "basic"
-    ],
-    "min_grid_dim": 3,
-    "max_grid_dim": 30,
-    "validated": True
-  },
-
+    task2 = {
+        "program": "[\n  sub(N+0.y, 1),\n  set_pixels(N+0, N+0.x, N+1, N+0.c),\n  del(N+1),\n  del(N+0),\n  set_pixels(N+0, N+0.x, N+0.max_y, 0),\n  del(N+0),\n  crop(N+0, 0, 1, N+0.width, N+0.height),\n  del(N+0),\n  equal(N+0.c, param1),\n  switch(N+1, param1, param2),\n  del(N+1),\n  set_pixels(N+0, N+0.x, N+0.y, N+1),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 25, 1, 61, 53, 2, 5, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 62, 2, 61, 54, 3],
+        [0, 51, 1, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 61, 56, 2, 4, 3],
+        [0, 51, 1, 61, 3],
+        [0, 36, 1, 61, 2, 4, 2, 5, 2, 61, 57, 2, 61, 58, 3],
+        [0, 51, 1, 61, 3],
+        [0, 18, 1, 61, 54, 2, "param1", 3],
+        [0, 21, 1, 62, 2, "param1", 2, "param2", 3],
+        [0, 51, 1, 62, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 61, 53, 2, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "y - 1",
+        "Grid",
+        "",
+        "",
+        "Grid",
+        "",
+        "Grid",
+        "",
+        "for each pixel, whether it has color 0",
+        "non-0 color pixels set to 1",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 1,
+        "name": "Shift Up + OtherColors",
+        "source": "Manual",
+        "parameters": [
+        "existing_color",
+        "color"
+        ],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
 
     is_same = run_test(task1, task2)
 
@@ -199,23 +199,260 @@ def test_compare_two_tasks():
 
     # Example #3: two distinct tasks
 
+    task1 = {
+        "program": "[\n  add(N+0.width, N+0.x),\n  set_pixels(N+0, N+1, N+0.y, N+0.c),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 24, 1, 61, 57, 2, 61, 52, 3],
+        [0, 38, 1, 61, 2, 62, 2, 61, 53, 2, 61, 54, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "Add grid width to its x values",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Tile Right",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 15,
+        "validated": True
+    }
+
+    task2 = {
+        "program": "[\n  add(N+0.height, N+0.y),\n  set_pixels(N+0, N+0.x, N+1, N+0.c),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 24, 1, 61, 58, 2, 61, 53, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 62, 2, 61, 54, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "Add grid height to its y values",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Tile Down",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 15,
+        "validated": True
+    }
+
     is_same = run_test(task1, task2)
 
     assert is_same is False
 
     # Example #4: two identical tasks (but slightly different code)
+    task1 = {
+        "name": "Draw Diagonal",
+        "program": "[\n  and(N+0.max_y, N+0.x),\n  set_pixels(N+0, N+0.x, N+1, param1),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 32, 1, 61, 56, 2, 61, 52, 3],
+        [0, 38, 1, 61, 2, 61, 52, 2, 62, 2, "param1", 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "grid height / 2",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "source": "Auto",
+        "parameters": [
+        "fg_color"
+        ],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+
+    task2 = {
+        "name": "Draw Diagonal #2",
+        "program": "[\n  and(N+0.max_y, N+0.x),\n  set_pixels(N+0, N+0.x, N+1, 3),\n  set_pixels(N+0, N+0.x, N+1, param1),\n  del(N+2),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        ],
+        "comments": [
+        "grid height / 2",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "source": "Auto",
+        "parameters": [
+        "fg_color"
+        ],
+        "grid_categories": [
+        "basic"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+    
+    prog2 = block_of_text_to_program_lines(task2['program'])
+    task2['instructions'] = ProgUtils.convert_user_format_to_token_seq(prog2)
 
     is_same = run_test(task1, task2)
 
     assert is_same is True
 
     # Example #5: two identical tasks (but slightly different code)
+    task1 =   {
+        "program": "[\n  get_objects(N+0),\n  del(N+0),\n  index(N+0, 0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 15, 1, 61, 3],
+        [0, 51, 1, 61, 3],
+        [0, 22, 1, 61, 2, 4, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "list of objects",
+        "",
+        "Grid",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Object Crop",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "single_object",
+        "single_object_noisy_bg"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+
+
+    task2 = {
+        "program": "[\n  get_objects(N+0),\n  del(N+0),\n  equal(N+0.c, 2),\n  equal(N+0.c, 3),\n  or(N+1, N+2),\n  index(N+0, 0),\n  del(N+1),\n  del(N+1),\n  del(N+1),\n  del(N+0)\n]",
+        "instructions": [
+        ],
+        "comments": [
+        "list of objects",
+        "",
+        "Grid",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Object Crop #2",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "single_object",
+        "single_object_noisy_bg"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+
+    prog2 = block_of_text_to_program_lines(task2['program'])
+    task2['instructions'] = ProgUtils.convert_user_format_to_token_seq(prog2)
 
     is_same = run_test(task1, task2)
 
     assert is_same is True
 
     # Example #6: two identical tasks (but slightly different code)
+    task1 =   {
+        "program": "[\n  get_objects(N+0),\n  get_bg(N+0),\n  del(N+0),\n  sub(N+0.max_x, N+0.x),\n  set_x(N+0, N+2),\n  del(N+0),\n  del(N+1),\n  rebuild_grid(N+0, N+1),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 15, 1, 61, 3],
+        [0, 16, 1, 61, 3],
+        [0, 51, 1, 61, 3],
+        [0, 25, 1, 61, 55, 2, 61, 52, 3],
+        [0, 39, 1, 61, 2, 63, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 62, 3],
+        [0, 48, 1, 61, 2, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "list of objects",
+        "grid background",
+        "",
+        "max_x - x",
+        "list of objects",
+        "",
+        "",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Object Flip H",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "non_symmetrical_shapes"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+
+    task2 =   {
+        "program": "[\n  get_objects(N+0),\n  get_bg(N+0),\n  del(N+0),\n  sub(N+0.max_x, N+0.x),\n  set_x(N+0, N+2),\n  set_y(N+0, N+0.y),\n  del(N+0),\n  del(N+1),\n  rebuild_grid(N+0, N+1),\n  del(N+2),\n  del(N+0),\n  del(N+0)\n]",
+        "instructions": [
+        [0, 15, 1, 61, 3],
+        [0, 16, 1, 61, 3],
+        [0, 51, 1, 61, 3],
+        [0, 25, 1, 61, 55, 2, 61, 52, 3],
+        [0, 39, 1, 61, 2, 63, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 62, 3],
+        [0, 48, 1, 61, 2, 62, 3],
+        [0, 51, 1, 61, 3],
+        [0, 51, 1, 61, 3]
+        ],
+        "comments": [
+        "list of objects",
+        "grid background",
+        "",
+        "max_x - x",
+        "list of objects",
+        "",
+        "",
+        "Grid",
+        "",
+        ""
+        ],
+        "curriculum_level": 0,
+        "name": "Object Flip H #2",
+        "source": "Manual",
+        "parameters": [],
+        "grid_categories": [
+        "non_symmetrical_shapes"
+        ],
+        "min_grid_dim": 3,
+        "max_grid_dim": 30,
+        "validated": True
+    }
+
+    prog2 = block_of_text_to_program_lines(task2['program'])
+    task2['instructions'] = ProgUtils.convert_user_format_to_token_seq(prog2)
 
     is_same = run_test(task1, task2)
 
